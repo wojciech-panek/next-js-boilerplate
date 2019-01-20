4e414b55000b954d2df1754eebda981399554128
Wojciech Panek <wpanek@apptension.com>
2019-01-20 13:49:57 +0100

    Add example favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,6 +21,18 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" key="viewport" />
+          <link rel="shortcut icon" href="/static/icons/favicon.ico" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+
+          <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
+
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          <link rel="manifest" href="/static/manifest.json" />
 
           {this.props.styleTags}
         </Head>
